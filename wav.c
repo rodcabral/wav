c6efb3efb90f61ca5d6b3b45f44ee70d0b6ba325
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2024-05-26 19:10:05 -0300

    full structure

--- wav.c
+++ wav.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 
 #define SAMPLE_RATE 44100
 #define BIT_DEPTH 16
-#define DURATION 2
 
 struct SineOscillator {
     float frequency;
@@ -13,75 +12,77 @@ struct SineOscillator {
     float offset;
 };
 
-struct SineOscillator *init_sine_oscillator(float freq, float amp) {
-    struct SineOscillator *so = malloc(sizeof(struct SineOscillator));
+struct SineOscillator* init_sine_oscillator(float freq, float amp) {
+    struct SineOscillator* so = malloc(sizeof(struct SineOscillator));
 
+    so->offset = 0.0f;
     so->frequency = freq;
     so->amplitude = amp;
     so->angle = 0.0f;
-    so->offset = 0.0f;
-    so->angle += so->amplitude * sin(2 * M_PI * so->frequency / SAMPLE_RATE);
+
     so->offset = 2 * M_PI * so->frequency / SAMPLE_RATE;
 
     return so;
+}
+
+float process_so(struct SineOscillator* so) {
+    float sample = so->amplitude * sin(so->angle);
+    so->angle += so->offset;
+    return sample;
 }
 
 void destroy_sine_oscillator(struct SineOscillator* so) {
     free(so);
 }
 
-float process(struct SineOscillator* so) {
-    float sample = so->amplitude * sin(so->angle);
-    so->angle += so->offset;
-    return sample;
+void write(FILE*fptr, int val, int size) {
+    fwrite((char*)&val, 1, size, fptr);
 }
 
-int main() {
-    struct SineOscillator *so = init_sine_oscillator(440, 0.5);
+int main(void) {
+    int duration = 2;
+    struct SineOscillator* so = init_sine_oscillator(440, 0.5);
 
     FILE* file = fopen("waveform.wav", "wb");
 
-    // Header chunk
+    // Header
     fwrite("RIFF", 1, 4, file);
     fwrite("----", 1, 4, file);
     fwrite("WAVE", 1, 4, file);
 
-    // Format chunk
+    // Format
     fwrite("fmt ", 1, 4, file);
-    int size = 16;
-    fwrite((char*)&size, 1, 4, file);
-    
-    int compression_code = 1;
-    fwrite((char*)&compression_code, 1, 2, file);
+    write(file, 16, 4);
+    write(file, 1, 2);
+    write(file, 1, 2);
+    write(file, SAMPLE_RATE, 4);
+    write(file, SAMPLE_RATE * BIT_DEPTH / 8, 4);
+    write(file, BIT_DEPTH / 8, 2);
+    write(file, BIT_DEPTH, 2);
 
-    int channels = 2;
-    fwrite((char*)&channels, 1, 2, file);
-
-    int sample_rate = 4;
-    fwrite((char*)&sample_rate, 1, 4, file);
-
-    int bit_rate = sample_rate * BIT_DEPTH / 8;
-    fwrite((char*)&bit_rate, 1, 4, file);
-
-    int block_align = BIT_DEPTH / 8;
-    fwrite((char*)&block_align, 1, 2, file);
-
-    int bit_depth = BIT_DEPTH;
-    fwrite((char*)&bit_depth, 1, 2, file);
-
-    // Data chunk
     fwrite("data", 1, 4, file);
     fwrite("----", 1, 4, file);
+    
+    int pre_audio_pos = ftell(file);
 
-    float max_amplitude = pow(2, BIT_DEPTH - 1) - 1;
-    for(int i = 0; i < SAMPLE_RATE * DURATION; ++i) {
-        float sample = process(so);
-        int int_sample = (int)(sample * max_amplitude);
-
-        fwrite((char*)&int_sample, 1, 2, file);
+    int max_amplitude = pow(2, BIT_DEPTH - 1) - 1;
+    for(int i = 0; i < SAMPLE_RATE * duration; ++i) {
+        float sample = process_so(so);
+        int int_sample = (int)(sample*max_amplitude);
+        write(file, int_sample, 2);
     }
+    int post_audio_pos = ftell(file);  
 
-    fclose(file);
+    fseek(file, pre_audio_pos - 4, SEEK_SET);
+    write(file, post_audio_pos - pre_audio_pos, 4);
+        
+    fseek(file, 4, SEEK_SET);
+    write(file, post_audio_pos - 8, 4);
+
+    printf("pre: %d\n", pre_audio_pos);
+    printf("pos: %d\n", post_audio_pos);
+
     destroy_sine_oscillator(so);
+    fclose(file);
     return 0;
 }
